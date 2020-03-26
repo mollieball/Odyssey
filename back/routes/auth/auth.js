86d650dedcf6f151c1d277f2f50bfb6f7519c61d
Mollie <mollieball@squarepeg.ie>
2020-03-26 16:36:46 +0000

    Create quest040

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,8 +1,31 @@
+const connection = require("../../helpers/db.js");
 const express = require("express");
 const router = express.Router();
+const bodyParser = require("body-parser");
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
 
 router.post("/signup", function(req, res, next) {
-  res.send("I am in POST signup");
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+    name: req.body.name,
+    lastname: req.body.lastname
+  };
+
+  connection.query("INSERT INTO users SET ?", user, function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) {
+      res.status(500).end();
+    } else {
+      res.status(200).json(user);
+      res.end();
+    }
+  });
 });
 
 module.exports = router;
