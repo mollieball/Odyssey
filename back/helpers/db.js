e8e216f17cb5c68298b4736353d408a33e685bb3
mollieball <59836532+mollieball@users.noreply.github.com>
2020-03-31 15:06:05 +0100

    Delete db.js

--- back/helpers/db.js
+++
@@ -1,8 +0,0 @@
-const mysql = require("mysql");
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Wardrobe-123",
-  database: "my_db"
-});
-module.exports = connection;
